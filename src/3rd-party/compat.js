1de57d7950de0c2cdcf67ca2fb8a0fa2637aabc3
DiamondYuan <fandi.yfd@antgroup.com>
2023-08-04 17:31:23 +0800

    feat: upgrate export

--- src/3rd-party/compat.js
+++ src/3rd-party/compat.js
@@ -1,6 +1,6 @@
 import * as compat from './node_modules/preact/compat/dist/compat.js';
-export { Children, PureComponent, StrictMode, Suspense, SuspenseList, __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED, cloneElement, createFactory, createPortal, findDOMNode, flushSync, forwardRef, hydrate, isValidElement, lazy, memo, render, startTransition, unmountComponentAtNode, unstable_batchedUpdates, useDeferredValue, useInsertionEffect, useSyncExternalStore, useTransition, version } from './node_modules/preact/compat/dist/compat.js';
-export { Component, Fragment, createContext, createElement, createRef } from './node_modules/preact/dist/preact.js';
+export { Children, PureComponent, StrictMode, Suspense, SuspenseList, cloneElement, createFactory, createPortal, findDOMNode, forwardRef, hydrate, isValidElement, lazy, memo, render, startTransition, unmountComponentAtNode, unstable_batchedUpdates, useDeferredValue, useInsertionEffect, useSyncExternalStore, useTransition, version } from './node_modules/preact/compat/dist/compat.js';
+export { Component, Fragment, createContext, createElement, createRef, createElement as h, options, toChildArray } from './node_modules/preact/dist/preact.js';
 export { useCallback, useContext, useDebugValue, useEffect, useErrorBoundary, useId, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState } from './node_modules/preact/hooks/dist/hooks.js';
 
 
---
+++ src/compat.ts
@@ -0,0 +1,1 @@
+export * from './3rd-party/compat.js';
