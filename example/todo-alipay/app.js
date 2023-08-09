3a48a3fd83b825d269b0edbb5c46873e521f7487
DiamondYuan <fandi.yfd@antgroup.com>
2023-08-09 16:53:15 +0800

    chore: update todo text

--- example/todo-alipay/app.js
+++ example/todo-alipay/app.js
@@ -1,7 +1,7 @@
 App({
   todos: [
     { text: 'Learning Javascript', completed: true },
-    { text: 'Learning ES2016', completed: true },
-    { text: 'Learning 支付宝小程序', completed: false },
+    { text: 'Learning 小程序开发', completed: true },
+    { text: 'Learning React 函数式组件', completed: false },
   ],
 });
